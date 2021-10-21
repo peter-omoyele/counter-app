da6dada701237b0172c5455c110e7597a2abcedc
Peter Omoyele <peteromoyele@gmail.com>
2021-10-21 16:09:27 +0100

    added project files

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+//initialize the count as 0
+// Listen for clicks on the increment button
+// Increment the count variable when the button is clicked
+// Chnage the count-el in the HTML
+
+let countEl = document.getElementById("count-el");
+let count = 0;
+let saveEl = document.getElementById("save-el")
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+};
+
+function save() {
+    let upCount = count + " - ";
+    saveEl.textContent += upCount;
+    countEl.textContent = 0;
+    count = 0;
+
+};
+
+
+
